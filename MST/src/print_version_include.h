d876d7cd438ecfa3b7be05adcdafb44d2cf41c68
Yang Wang <ywg@bolt.psc.edu>
2020-05-27 13:52:34 -0400

    A minor fix to the previous commit.

--- MST/src/print_version_include.h
+++
@@ -1,7 +0,0 @@
-! 
-! this routine is automatically generated at compilation time
-! DO NOT EDIT 
-! 
-   write(myunit,'("# ",t5,a,t30,": ",a)') "username",trim("ywg")
-   write(myunit,'("# ",t5,a,t30,": ",a)') "system name",trim("bolt.psc.edu")
-   write(myunit,'("# ",t5,a,t30,": ",a)') "code compiled",trim("Wednesday, May 27 2020 13:39:43")
