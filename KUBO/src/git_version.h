697a1aaa94eb93220fcc9f8faa8c5de6d9a10c19
Yang Wang <ywg@psc.edu>
2024-05-24 13:50:40 -0400

    Fixed a minor issue that could cause git pull command to fail.

--- KUBO/src/git_version.h
+++
@@ -1,1 +0,0 @@
-write(myunit,'("# ",t5,a,t30,": ",a)') "MuST source code version","v1.9.1.3"
--- KUBO/src/print_version_include.h
+++
@@ -1,7 +0,0 @@
-! 
-! this routine is automatically generated at compilation time
-! DO NOT EDIT 
-! 
-   write(myunit,'("# ",t5,a,t30,": ",a)') "username",trim("ywg")
-   write(myunit,'("# ",t5,a,t30,": ",a)') "system name",trim("a001.ib.bridges2.psc.edu")
-   write(myunit,'("# ",t5,a,t30,": ",a)') "code compiled",trim("Monday, April 29 2024 18:43:51")
